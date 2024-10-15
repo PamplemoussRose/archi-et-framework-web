c2f3a1ffc2cb0f689e659b2cae98e7b81ff7f8e2
PamplemoussRose <robin49800@gmail.com>
2024-10-15 17:02:40 +0200

    Entités

---
+++ polytech/polytech/src/main/java/com/capgemini/polytech/entite/Terrain.java
@@ -0,0 +1,63 @@
+package com.capgemini.polytech.entite;
+
+import jakarta.persistence.*;
+
+@Entity(name = "terrain")
+@Table(name = "TERRAIN")
+public class Terrain {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    @Column(name = "nom", nullable=false)
+    private String nom;
+
+    @Column(name = "quantite", nullable=false)
+    private int quantite;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "point_geo", nullable=false)
+    private String point_geo;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public int getQuantite() {
+        return quantite;
+    }
+
+    public void setQuantite(int quantite) {
+        this.quantite = quantite;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPoint_geo() {
+        return point_geo;
+    }
+
+    public void setPoint_geo(String point_geo) {
+        this.point_geo = point_geo;
+    }
+}
---
+++ polytech/polytech/src/main/java/com/capgemini/polytech/entite/Utilisateur.java
@@ -0,0 +1,74 @@
+package com.capgemini.polytech.entite;
+
+import jakarta.persistence.*;
+
+@Entity(name = "utilisateur")
+@Table(name = "UTILISATEUR")
+public class Utilisateur {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    @Column(name = "nom", nullable=false)
+    private String nom;
+
+    @Column(name = "nom", nullable=false)
+    private String prenom;
+
+    @Column(name = "nom", nullable=false)
+    private String email;
+
+    @Column(name = "nom", nullable=false)
+    private String password;
+
+    @Column(name = "nom", nullable=false)
+    private String username;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
